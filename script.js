2742d98e2dcd0eea5ea0cfe54ad7b842df331061
Cornell van der Straaten <30183@ma-web.nl>
2021-01-08 21:46:03 +0100

    Multiple

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 new fullpage('#fullpage', {
   navigation: true,
   responsiveWidth: 700,
-  });
+  afterLoad: function(){
+    //slide 2
+    if (document.getElementById('body').classList.contains('fp-viewing-1')) {
+      let s2TopText = document.querySelector('.s-2__top-text');
+      let s2BottomText = document.querySelector('.s-2__bottom-text');
+      
+      s2TopText.style.opacity = 1;
+
+      setTimeout( function() {
+        s2BottomText.style.opacity = 1;
+      }, 1000);
+    } 
+    // slide 3
+    if (document.getElementById('body').classList.contains('fp-viewing-2')) {
+      let s3TopText = document.querySelector('.s-3__top-text');
+      let s3BottomText = document.querySelector('.s-3__bottom-text');
+      
+      s3TopText.style.opacity = 1;
+
+      setTimeout( function() {
+        s3BottomText.style.opacity = 1;
+      }, 1000);
+    } 
+    //slide 4
+    if (document.getElementById('body').classList.contains('fp-viewing-3')) {
+      let s4TopText = document.querySelector('.s-4__top-text');
+      let s4BottomText = document.querySelector('.s-4__bottom-text');
+      
+      s4TopText.style.opacity = 1;
+
+      setTimeout( function() {
+        s4BottomText.style.opacity = 1;
+      }, 1000);
+    } 
+}
+});
 
 window.addEventListener("load", function(){
   let s1Logo = document.querySelector('.s-1__logo');
   let s1H1Text = document.querySelector('.s-1__h1-text');
   let s1PText = document.querySelector('.s-1__p-text');
   let s1ImageDiv = document.querySelector('.s-1__image-div');
+
   s1Logo.style.opacity = 1;
+
   setTimeout( function() {
     s1H1Text.style.opacity = 1;
   }, 1000);
@@ -22,3 +59,4 @@ window.addEventListener("load", function(){
     s1ImageDiv.style.opacity = 1;
   }, 2800);
 });
+
